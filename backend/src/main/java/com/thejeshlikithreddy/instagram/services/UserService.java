5a2a8b33f816867e9f5e0255a1dcfddf067771d1
LikithReddyA <alikithreddy381@gmail.com>
2024-11-23 19:50:24 +0530

    UsersService file name updated

--- backend/src/main/java/com/thejeshlikithreddy/instagram/services/UserService.java
+++ backend/src/main/java/com/thejeshlikithreddy/instagram/services/UserService.java
@@ -3,15 +3,27 @@ package com.thejeshlikithreddy.instagram.services;
 import com.thejeshlikithreddy.instagram.model.User;
 import com.thejeshlikithreddy.instagram.repository.UsersRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
 @Service
-public class UsersService {
+public class UserService {
   @Autowired
   private UsersRepo usersRepo;
+
+  public String userRegistration(User user){
+    BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder(12);
+    User existingUser = usersRepo.getByUser(user.getUser()).orElseGet(()->usersRepo.getByEmailPhone(user.getEmailPhone()).orElse(null));
+    if (existingUser != null){
+      return "User already exists!!!";
+    }
+    user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+    usersRepo.save(user);
+    return "User registered successfully!!!";
+  }
 
   public List<User> getUsers() {
     return usersRepo.findAll();
